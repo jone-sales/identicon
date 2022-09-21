b4f07a1510566d5c18dde5be1a724a4fc54a9426
consoelo <johnnysales_23@hotmail.com>
2022-09-21 19:59:02 -0300

    adds a filter odds func to paint the squares

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,6 +8,7 @@ defmodule Identicon do
     |>hash_input
     |>pick_color
     |>build_grid
+    |>filter_odds_squares
   end  
 
   def hash_input(input) do
@@ -36,5 +37,13 @@ defmodule Identicon do
   def mirror_row(row) do
     [first, second | _tail] = row
     row ++ [second, first]
+  end
+
+  def filter_odds_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+  
+    %Identicon.Image{image | grid: grid}  
   end
 end
