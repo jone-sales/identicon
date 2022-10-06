69412e6776aab915dd0436fb5095f851c4b48319
mew <rd_mew@outlook.com>
2022-10-06 16:20:13 -0300

    builds image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,6 +9,9 @@ defmodule Identicon do
     |>pick_color
     |>build_grid
     |>filter_odds_squares
+    |>build_pixel_map
+    |>draw_image
+    |>save_image(input)
   end  
 
   def hash_input(input) do
@@ -46,4 +49,34 @@ defmodule Identicon do
   
     %Identicon.Image{image | grid: grid}  
   end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50 
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      botton_right = {horizontal + 50, vertical + 50}
+
+      {top_left, botton_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
 end
