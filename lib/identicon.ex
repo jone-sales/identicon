a2686e2df441a3fd75ab8e9a83ed112a86b45b26
“mew” <johnnysales_23@hotmail.com>
2023-01-16 08:42:34 -0300

    Fix outdated functions.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -12,31 +12,31 @@ defmodule Identicon do
     |>build_pixel_map
     |>draw_image
     |>save_image(input)
-  end  
+  end
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
 
-    %Identicon.Image{hex: hex}    
+    %Identicon.Image{hex: hex}
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
-  
+
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    grid = 
+    grid =
       hex
-      |>Enum.chunk(3)
+      |>Enum.chunk_every(3)
       |>Enum.map(&mirror_row/1)
       |>List.flatten
       |>Enum.with_index
 
     %Identicon.Image{image | grid: grid}
 
-  end  
-  
+  end
+
   def mirror_row(row) do
     [first, second | _tail] = row
     row ++ [second, first]
@@ -46,13 +46,13 @@ defmodule Identicon do
     grid = Enum.filter grid, fn({code, _index}) ->
       rem(code, 2) == 0
     end
-  
-    %Identicon.Image{image | grid: grid}  
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
     pixel_map = Enum.map grid, fn({_code, index}) ->
-      horizontal = rem(index, 5) * 50 
+      horizontal = rem(index, 5) * 50
       vertical = div(index, 5) * 50
 
       top_left = {horizontal, vertical}
